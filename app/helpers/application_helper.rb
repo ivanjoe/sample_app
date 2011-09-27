58d151c13bc2f8087218ada524a457c543eb3c7e
Ivan Kubinyi <ikubinyi@gmail.com>
2011-09-27 23:57:44 +0300

    End of Chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  # Return the URL of the logo picture
+  def logo
+    @logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
   
   # Return a title on a per-page basis.               # Documentation comment
   def title                                           # Method definition
